083188258000fee705800e23c6b6cdee28e7cd43
RedPandaGA <103851430+RedPandaGA@users.noreply.github.com>
2022-09-27 00:12:12 -0500

    Working int2bitstr

--- prog1/int2bitstr.c
+++ prog1/int2bitstr.c
@@ -1,18 +1,20 @@
 int int2bitstr(int I, char *str) {
 	int counter = 0;
+	str[32]='\0';
 	for(int position = 0; position < 32; position++){
 		unsigned int bit = (I >> position) & 1;
 		if(bit == 1){
 			counter++;
-			str[31-position] = 1;
+			str[31-position] = '1';
 		} else {
-			str[31-position] = 0;
+			str[31-position] = '0';
 		}
 	}
+	return counter;
 }
 
 int get_exp_value(float f) {
 	unsigned f2u(float f);
 	unsigned int ui = f2u(f);
-	/* implment here */
+	return 0;
 }
